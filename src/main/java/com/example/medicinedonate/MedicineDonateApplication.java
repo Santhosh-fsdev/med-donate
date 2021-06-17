b09556baa66530fe5d164142db418b3d079aa8fc
Santhosh Kumar <sandymech290023@gmail.com>
2021-06-17 21:50:39 +0530

    adding args

--- src/main/java/com/example/medicinedonate/MedicineDonateApplication.java
+++ src/main/java/com/example/medicinedonate/MedicineDonateApplication.java
@@ -11,11 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicineDonateApplication {
 
-    @Autowired
-    UserRepository userRepository;
 
 	public static void main(String[] args) {
+		String context = "";
+		for(String arg:args) {
+			if(arg.contains("contextroot")){
+				String array[] =  arg.split("=",2);
+				context = array[1];
+			}
+      }
 		
+		System.setProperty("server.servlet.context-path", context);
 		SpringApplication.run(MedicineDonateApplication.class, args);
 	}
 
